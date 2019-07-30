86d0bceb2a7e9622aab6821948e214d314fac5db
Michael Karanja <webmechapps1@gmail.com>
2019-07-30 22:06:32 +0300

    Add exercise.py
    The code will be optimized later to increase efficiency

---
+++ arrays_exercise1.py
@@ -0,0 +1,107 @@
+"""1. You have been given an array of distinct integer values, No number in the array is repeated. 
+It also gives you a separate integer value that represents a target sum. Write a function that is 
+going to find whether or not there is a pair of numbers in the array given, that sums up to the target sum.
+[3, 5, -4, 8, 11, 1, -1, 6]  target sum  = 10"""
+
+def pair_sum_is_n(arr, n):
+    """Return True if 2 numbers from `arr` add up to n, False otherwise.
+
+    Runtime:  O(N) -- linear runtime
+    """
+    arr = set(arr)
+    for i in arr: # i is one part of the pair
+        if n - i in arr: # n-i is the second part of the pair; checking if it exists
+            return True
+    return False
+
+#print(pair_sum_is_n([3, 5, -4, 8, 11, 1, -1], 17))
+
+"""
+2.  You have been given an array of distinct integer values, No number in the array is repeated. 
+It also gives you a separate integer value that represents a target sum write  a function that 
+will find all the possible triplets in the array, that sum up to the target sum
+    [12, 3, 1, 2, -6, 5, -8, 6] target sum  = 0
+
+"""
+def triplet_sum_is_n(arr, n): 
+    """
+    Return a triplet from array 'arr' whose sum is n
+    Runtime:
+        O(N^2)"""
+    for i in range(0, len(arr)): 
+        # Find pair in subarray arr[i + 1..n-1]  
+        # with sum equal to sum - arr[i] 
+        s = set() # ensuring no duplicates
+        current_sum = n - arr[i] 
+        for j in range(i + 1, len(arr)): 
+            if (current_sum - arr[j]) in s: 
+                return True
+            s.add(arr[j]) 
+            print("Triplet:", arr[i],  
+                        ", ", arr[j], ", ", current_sum-arr[j])
+    return False
+
+#print(triplet_sum_is_n([12, 3, 1, 2, -6, 5, -8, 6], 15))
+
+
+"""
+3.  You have two arrays of integer values. Write a function to find the pair 
+of numbers where one number comes from the first array,
+and another number comes from the second array with the smallest difference.
+Array A = [-1, 5, 10, 20, 28, 3]
+Array B = [26, 134, 135, 15, 17]"""
+
+
+def smallest_difference_pair(arr1, arr2): 
+    # sorting the arrays 
+    arr1.sort() 
+    arr2.sort() 
+    
+    res_min = 0; res_max = 0 # To store resultant 2 numbers 
+
+    i, j = 0,0  # pointers to arr1, arr2, 
+    # Loop until one array reaches to its end 
+    # Find the smallest difference. 
+    diff = 2147483647 # max size for a 32-bit system (sys.maxsize)
+    while (i < len(arr1) and j < len(arr1)): 
+        maxi = max(arr1[i], arr2[j])  # maximum number 
+        mini = min(arr1[i], arr2[j]) # Find minimum and increment its index.
+        if (mini == arr1[i]): 
+            i += 1
+        else: 
+            j += 1
+        # Comparing new difference with the 
+        # previous one and updating accordingly 
+        if (diff > (maxi - mini)): 
+            diff = maxi - mini
+            res_max = maxi
+            res_min = mini
+    return res_max, res_min
+
+#print(smallest_difference_pair([-1, 5, 10, 20, 29, 3], [26, 134, 135, 15, 19]))
+
+"""
+4. You have been given an array of distinct integer values, No number in the array is repeated. 
+It also gives you a separate integer value that represents a target sum. write  a function that 
+will find all the possible quadruplets in the array, that sum up to the target number.
+[7, 6, 4, -1, 1, 2] target sum  = 16 """
+
+def quadruplets_sum_to_target(arr, n): 
+    """
+    O(N^3)
+    """  
+    # Fix the first element and find  
+    # other three 
+    for i in range(0,len(arr)-3): 
+          
+        # Fix the second element and  
+        # find other two 
+        for j in range(i+1,len(arr)-2): 
+              
+            # Fix the third element  
+            # and find the fourth 
+            for k in range(j+1,len(arr)-1): 
+                if n - (arr[i] + arr[j] + arr[k]) in arr: # finding if the remainder exists
+                    print("Quadruplet:", arr[i], arr[j], arr[k], n - (arr[i] + arr[j] + arr[k]) )
+
+#print(quadruplets_sum_to_target([ 7, 6, 4, -1, 1, 2, 8, 8, 0, 0], 16))
